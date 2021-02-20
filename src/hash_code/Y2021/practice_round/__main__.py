4dccda2da716f89fb2b4cca7627afef52b91ed13
mazghal <aminezghal@vente-privee.com>
2021-02-20 21:18:48 +0100

    add waste optimisation

--- src/hash_code/Y2021/practice_round/__main__.py
+++ src/hash_code/Y2021/practice_round/__main__.py
@@ -1,4 +1,4 @@
-
+import logging
 import os
 from src.hash_code.Y2021.practice_round.solution import make_solution
 from src.hash_code.Y2021.practice_round.tools import format_input
@@ -7,15 +7,24 @@ from src.hash_code.tools import read_file
 from src.hash_code.tools import write_file
 
 if __name__ == '__main__':
-    file_name = "d_many_pizzas.in"
-    in_path = os.path.join("../../../../data/Y2021/practice_round/", file_name)
-    out_path = os.path.join("../../../../submissions/Y2021/practice_round/", file_name)
-    in_content = read_file(path=in_path)
-    nb_pizza, nb_t2, nb_t3, nb_t4, pizzas = format_input(content=in_content)
-    orders = make_solution(nb_pizza=nb_pizza,
-                           nb_t2=nb_t2,
-                           nb_t3=nb_t3,
-                           nb_t4=nb_t4,
-                           pizzas=pizzas)
-    out_content = format_output(orders)
-    write_file(content=out_content, path=out_path)
+    files = [
+        "a_example",
+        "b_little_bit_of_everything.in",
+        "c_many_ingredients.in",
+        "d_many_pizzas.in",
+        "e_many_teams.in"
+    ]
+    for file_name in files:
+        logger = logging.getLogger()
+        logger.info(f'Solving {file_name}')
+        in_path = os.path.join("../../../../data/Y2021/practice_round/", file_name)
+        out_path = os.path.join("../../../../submissions/Y2021/practice_round/", file_name)
+        in_content = read_file(path=in_path)
+        nb_pizza, nb_t2, nb_t3, nb_t4, pizzas = format_input(content=in_content)
+        orders = make_solution(nb_pizza=nb_pizza,
+                               nb_t2=nb_t2,
+                               nb_t3=nb_t3,
+                               nb_t4=nb_t4,
+                               pizzas=pizzas)
+        out_content = format_output(orders)
+        write_file(content=out_content, path=out_path)
--- src/hash_code/Y2021/practice_round/solution.py
+++ src/hash_code/Y2021/practice_round/solution.py
@@ -35,13 +35,15 @@ class Storage:
         return sum([value for key, value in self.pizzas.items()])
 
     def get_pizza(self, excluded):
-        best_pizza, best_value = None, 0
+        best_pizza, best_value, least_ingredients = None, -1, 0
         for pizza in self.recipes.values():
             value = 0
             for ingredient in pizza:
                 if ingredient not in excluded:
                     value += 1
-            if value >= best_value:
+            if value > best_value:
+                best_pizza, best_value = pizza, value
+            elif value == best_value and len(pizza) < least_ingredients:
                 best_pizza, best_value = pizza, value
         return best_pizza
 
@@ -51,29 +53,49 @@ def make_solution(nb_pizza, nb_t2, nb_t3, nb_t4, pizzas):
     logger.info(f'Preparing {nb_pizza} pizzas')
     orders = list()
     storage = Storage()
-    for pizza in tqdm(pizzas):
+    for pizza in tqdm(pizzas, desc="Storage progress"):
         storage.add_pizza(pizza=pizza)
     assert nb_pizza == storage.get_remaining_pizzas()
-    teams = sorted([2 for _ in range(nb_t2)] \
-                   + [3 for _ in range(nb_t3)] \
-                   + [4 for _ in range(nb_t4)])
-    logger.info(f'Fulfilling orders: {nb_t2} teams of 2, {nb_t3} teams of 3, and {nb_t4} teams of 4.')
-    p_bar = tqdm(total=len(teams))
-    while len(teams) > 0:
-        size = teams.pop(-1)
-        if size <= storage.get_remaining_pizzas():
-            order = fulfill_order(size=size, storage=storage)
-            orders.append(order)
-        p_bar.update(1)
+    logger.info(f'Fulfilling orders: {nb_t2} teams of 2, '
+                f'{nb_t3} teams of 3, and {nb_t4} teams of 4.')
+    pizza_bar = tqdm(total=nb_pizza, desc="Pizza consumption progress")
+    total_score = 0
+    teams = {
+        2: nb_t2,
+        3: nb_t3,
+        4: nb_t4
+    }
+    teams_bar = {key: tqdm(total=value, desc=f'Teams of {key} progress')
+                 for key, value in teams.items()}
+    remaining_pizzas = storage.get_remaining_pizzas()
+    sizes = [key for key, value in teams.items()
+             if value > 0 and key <= remaining_pizzas]
+    while nb_t2 + nb_t3 + nb_t4 > 0 and len(sizes) > 0:
+        order, score = fulfill_order(sizes=sizes, storage=storage)
+        size = len(order)
+        orders.append(order)
+        pizza_bar.update(size)
+        teams_bar[size].update(1)
+        total_score += score ** 2
+        teams[size] -= 1
+        remaining_pizzas = storage.get_remaining_pizzas()
+        sizes = [key for key, value in teams.items()
+                 if value > 0 and key <= remaining_pizzas]
+    logger.info(f'Collected {total_score} points')
     return orders
 
 
-def fulfill_order(size, storage):
+def fulfill_order(sizes, storage):
     order = list()
     used_ingredients = set()
-    for _ in range(size):
+    score = 0
+    for index in range(max(sizes)):
         pizza = storage.get_pizza(excluded=used_ingredients)
-        id = storage.remove_pizza(pizza=pizza)
-        order.append(id)
         used_ingredients.update(pizza)
-    return order
+        if len(used_ingredients) == score and len(order) in sizes:
+            break
+        else:
+            id = storage.remove_pizza(pizza=pizza)
+            order.append(id)
+            score = len(used_ingredients)
+    return order, score
