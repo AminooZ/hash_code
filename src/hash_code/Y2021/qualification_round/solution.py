e266c30643d8ae96acd1ed36e258c444b6ed5cb1
mazghal <aminezghal@vente-privee.com>
2021-02-25 20:24:56 +0100

    fist submission

--- src/hash_code/Y2021/qualification_round/solution.py
+++ src/hash_code/Y2021/qualification_round/solution.py
@@ -1,10 +1,56 @@
 import logging
 import os
+from collections import defaultdict
+from math import ceil
 
-if __name__ == '__main__':
-    file_name = "toto"
+
+class City:
+    def __init__(self, ):
+        self.in_intersections = defaultdict(list)
+        self.out_intersections = defaultdict(list)
+        self.streets = dict()
+        self.traffic = defaultdict(int)
+        self.weights = dict()
+
+    def add_street(self, street):
+        self.in_intersections[street["end"]].append(street["name"])
+        self.out_intersections[street["start"]].append(street["name"])
+        self.streets[street["name"]] = street
+
+    def add_car(self, car):
+        for street in car:
+            self.traffic[street] += 1
+
+    def build_weights(self):
+        for intersection, streets in self.in_intersections.items():
+            total_traffic = sum([self.traffic[street] for street in streets])
+            self.weights[intersection] = {
+                street: self.traffic[street] / total_traffic for street in streets
+            }
+
+    def build_scheduler_v0(self, lift=2):
+        scheduler = {
+            intersection: {
+                street: ceil(lift * weight / sum(traffic.values()))
+                for street, weight in traffic.items()
+            }
+            for intersection, traffic in self.weights.items()
+        }
+        return scheduler
+
+
+def make_solution(total_time, nb_intersection, reward, streets, cars):
     logger = logging.getLogger()
-    logger.info(f'Solving {file_name}')
-    in_path = os.path.join("../../../../data/Y2021/practice_round/", file_name)
-    out_path = os.path.join("../../../../submissions/Y2021/practice_round/", file_name)
-    print("hello world")
+    city = City()
+    logger.info(f'Adding {len(streets)} streets')
+    for street in streets:
+        city.add_street(street=street)
+    logger.info(f'Adding {len(cars)} cars')
+    for car in cars:
+        city.add_car(car=car)
+    logger.info(f'Building weights')
+    city.build_weights()
+    logger.info(f'Building scheduler')
+    scheduler = city.build_scheduler_v0(lift=2)
+    return scheduler
+
--- src/hash_code/Y2021/qualification_round/tools.py
+++ src/hash_code/Y2021/qualification_round/tools.py
@@ -1,26 +1,56 @@
 import logging
+import os
+
+from hash_code.tools import read_file
 
 
 def format_input(content):
     logger = logging.getLogger()
     logger.info(f'Formatting input')
     header, body = content[0], content[1:]
-    nb_pizza, nb_t2, nb_t3, nb_t4 = [int(x) for x in header]
-    pizzas = list()
-    for line in body:
-        nb_ingredients, ingredients = int(line[0]), line[1:]
-        assert len(ingredients) == nb_ingredients
-        pizzas.append(ingredients)
-    return nb_pizza, nb_t2, nb_t3, nb_t4, pizzas
+    total_time, nb_intersection, nb_street, nb_car, reward = [int(x) for x in header]
+    raw_streets, raw_cars = body[:nb_street], body[nb_street:]
+    streets = [
+        {
+            "start": int(start),
+            "end": int(end),
+            "name": name,
+            "length": length
+        }
+        for start, end, name, length in raw_streets
+    ]
+    cars = [trip[1:] for trip in raw_cars]
+    assert nb_street == len(streets)
+    assert nb_car == len(cars)
+    return total_time, nb_intersection, reward, streets, cars
 
 
-def format_output(orders, separator=" "):
+def format_output(scheduler, separator=" "):
     logger = logging.getLogger()
     logger.info(f'Formatting output')
-    header = str(len(orders))
+    header = str(len(scheduler.keys()))
     body = list()
-    for order in orders:
-        line = [str(len(order))] + [str(pizza) for pizza in order]
-        body.append(separator.join(line))
+    for intersection, streets in scheduler.items():
+        body.append(str(intersection))
+        body.append(str(len(streets)))
+        for street, time in streets.items():
+            body.append(separator.join([street, str(time)]))
     content = [header] + body
     return content
+
+
+#if __name__ == '__main__':
+    # file_name = "a.txt"
+    # logger = logging.getLogger()
+    # logger.info(f'Solving {file_name}')
+    # in_path = os.path.join("../../../../data/Y2021/qualification_round/", file_name)
+    # out_path = os.path.join("../../../../submissions/Y2021/qualification_round/",
+    #                         file_name)
+    # in_content = read_file(path=in_path)
+    # total_time, nb_intersection, reward, streets, cars = format_input(in_content)
+    # print(total_time)
+    # print(nb_intersection)
+    # print(reward)
+    # print(streets)
+    # print(cars)
+
